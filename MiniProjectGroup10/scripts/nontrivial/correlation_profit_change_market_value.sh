72186417be33e776c6b6b048783cf277e3702b02
Nathan Pham <nghiapham102603@gmail.com>
2023-11-17 16:31:40 -0800

    Collect nontrivial information

--- MiniProjectGroup10/scripts/nontrivial/correlation_profit_change_market_value.sh
+++ MiniProjectGroup10/scripts/nontrivial/correlation_profit_change_market_value.sh
@@ -4,13 +4,16 @@
 file_path="./data/Fortune 1000 Companies by Revenue.csv"
 
 # Use Python to calculate the correlation
-python - <<EOF
+python3 - <<EOF
 import pandas as pd
 
 df = pd.read_csv("$file_path")
-df['profits_percent_change'] = df['profits_percent_change'].replace('[\%,]', '', regex=True).astype(float)
-df['market_value'] = df['market_value'].replace('[\$,]', '', regex=True).astype(float)
-correlation = df['profits_percent_change'].corr(df['market_value'])
+# Replace non-numeric strings with NaN and convert the column to floats
+df['profits_percent_change'] = pd.to_numeric(df['profits_percent_change'].replace('[\%,]', '', regex=True), errors='coerce')
+df['market_value '] = pd.to_numeric(df['market_value '].replace('[\$,]', '', regex=True), errors='coerce')
+
+# Calculate the correlation, ignoring NaN values
+correlation = df['profits_percent_change'].corr(df['market_value '])
 print(f"Correlation between Profits Percent Change and Market Value: {correlation}")
 EOF
 
--- MiniProjectGroup10/scripts/nontrivial/correlation_revenue_employees.sh
+++ MiniProjectGroup10/scripts/nontrivial/correlation_revenue_employees.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Path to the CSV file
-file_path="./data/Fortune 1000 Companies by Revenue.csv"
+# Path to the CSV file, assuming the script is run from the MiniProjectGroup10 directory
+file_path="data/Fortune 1000 Companies by Revenue.csv"
 
 # Use Python to calculate the correlation
-python - <<EOF
+python3 - <<EOF
 import pandas as pd
 
 df = pd.read_csv("$file_path")
-df['revenues'] = df['revenues'].replace('[\$,]', '', regex=True).astype(float)
-df['employees'] = df['employees'].replace('[\$,]', '', regex=True).astype(float)
-correlation = df['revenues'].corr(df['employees'])
+
+df['revenues '] = pd.to_numeric(df['revenues '].str.replace('[\$,]', '', regex=True), errors='coerce')
+df['employees '] = pd.to_numeric(df['employees '].str.replace('[\$,]', '', regex=True), errors='coerce')
+
+correlation = df['revenues '].corr(df['employees '])
 print(f"Correlation between Revenues and Employees: {correlation}")
 EOF
 
